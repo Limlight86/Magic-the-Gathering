a333ef6697b9c7dc7a17f4d86025898f20294a3a
Lazaro <lquin008@gmail.com>
2020-02-18 12:27:32 -0500

    updated images for dual faced cards

--- src/components/CardDetails/CardDetails.jsx
+++ src/components/CardDetails/CardDetails.jsx
@@ -22,7 +22,9 @@ const CardDetails = (props) => {
   return(
     <div>
       <h1>{card.name}</h1>
-      {card.image_uris && <img src={card.image_uris.normal} alt={card.name}/>}
+      { card.image_uris && <img src={card.image_uris.normal} alt={card.name}/> }
+      { card.card_faces && <img src={card.card_faces[0].image_uris.normal} alt={card.name}/> }
+      { card.card_faces && <img src={card.card_faces[1].image_uris.normal} alt={card.name}/> }
     </div>
 )}
 
