84b30a9b8dc169b11d2c954ff45aa87007a51845
Lazaro <lquin008@gmail.com>
2020-08-22 09:02:58 -0400

    cleaned up queryApi

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -6,10 +6,9 @@ const AppContextProvider = ({ children }) => {
   const [apiResponse, setApiResponse] = useState([])
 
   const queryApi = async (term) => {
-    console.log(term)
     const response = await fetch(`/api/search/${term}`)
     const result = await response.json()
-    await setApiResponse(result)
+    setApiResponse(result)
     }
 
   return(
