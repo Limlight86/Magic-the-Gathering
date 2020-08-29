d1cde91a2139863400498df736ec5237e89c40c2
Lazaro <lquin008@gmail.com>
2020-08-29 14:21:04 -0400

    updates to include current deck functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "react-router-dom"
+import { AppContext } from "../../context/AppContext"
+import styles from "./Navbar.module.css"
 
 const Navbar = ()=>{
+  const { deckBuild, discardDeckInProgress} = useContext(AppContext)
 
   return(
     <nav>
       <Link to="/search">Search</Link>
+      {
+        deckBuild.length > 0 ? 
+        <div className={styles.currentDeckNav}>
+          <button onClick={discardDeckInProgress}>Discard Current Deck</button>
+          <Link to="/currentDeck">Current Deck</Link>
+        </div>
+        :
+        null 
+      }
     </nav>
   )
 }
