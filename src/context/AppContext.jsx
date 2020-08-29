a0e886c99d9b446c42a0e20c3c54255410970307
Lazaro <lquin008@gmail.com>
2020-08-29 13:06:59 -0400

    added localstorage and addCard to deck functionality

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,9 +1,10 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [apiResponse, setApiResponse] = useState([])
+  const [deckBuild, setDeckBuild] = useState([])
 
   const queryApi = async (term) => {
     const response = await fetch(`/api/search/${term}`)
@@ -11,9 +12,42 @@ const AppContextProvider = ({ children }) => {
     setApiResponse(result)
     }
 
+  const addCardToDeck = (e, card) => {
+    e.preventDefault()
+    const currentDeck = deckBuild
+    const cardInDeck = currentDeck.find(theCard => theCard.id === card.id)
+    if (cardInDeck){
+      const id = currentDeck.findIndex(theCard => theCard.id === card.id)
+      currentDeck[id].count += 1
+      setDeckBuild([...currentDeck])
+    } else {
+      setDeckBuild([...currentDeck, {...card, count: 1}])
+    }
+  }
+
+  useEffect(()=> {
+    if(deckBuild.length > 0){
+      localStorage.setItem("deckInProgress", JSON.stringify(deckBuild));
+    }
+  }, [deckBuild])
+
+  useEffect(() => {
+    const getDeckInProgress = async () => {
+      const deckInProgress = localStorage.getItem("deckInProgress")
+      console.log(deckInProgress)
+      const result =  await JSON.parse(deckInProgress)
+      if(typeof result === "object" && result !== null ){
+        console.log(result)
+        setDeckBuild([...result])
+      } 
+    }
+    getDeckInProgress()
+  }, [])
+
   return(
     <AppContext.Provider 
-      value={{apiResponse, setApiResponse, queryApi}}>
+      value={{apiResponse, setApiResponse, queryApi, deckBuild, setDeckBuild, addCardToDeck}}
+    >
       { children }
     </AppContext.Provider>
   )
