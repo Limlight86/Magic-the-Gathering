8fd3067c5d0492f6ddb9dc9595929380b3cdba37
Lazaro <lquin008@gmail.com>
2020-08-29 14:10:20 -0400

    added currentDeck to index component routing

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as SearchForm } from "./SearchForm/SearchForm";
 export { default as SearchResultGrid } from "./SearchResultGrid/SearchResultGrid";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as CardDetails } from "./CardDetails/CardDetails";
+export { default as CurrentDeck } from "./CurrentDeck/CurrentDeck";
