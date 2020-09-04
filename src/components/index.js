429d321037a98b0f849ab3c7b27c1c60711d122e
Lazaro <lquin008@gmail.com>
2020-09-04 16:40:24 -0400

    updated for new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as SearchResultGrid } from "./SearchResultGrid/SearchResultGrid
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as CardDetails } from "./CardDetails/CardDetails";
 export { default as CurrentDeck } from "./CurrentDeck/CurrentDeck";
+export { default as CastingCost } from "./CastingCost/CastingCost";
+export { default as CardImage } from "./CardImage/CardImage";
