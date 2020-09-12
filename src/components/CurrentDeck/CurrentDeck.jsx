4ebc4ab01dc54a2bb53099a0de58106f3bcb69d8
Lazaro <lquin008@gmail.com>
2020-09-11 23:53:04 -0400

    created sections for card types and styled components

--- src/components/CurrentDeck/CurrentDeck.jsx
+++ src/components/CurrentDeck/CurrentDeck.jsx
@@ -1,23 +1,44 @@
-import React, { useContext } from 'react';
-import { AppContext } from "../../context/"
+import React, { useContext } from "react";
+import { AppContext } from "../../context/";
+import { CastingCost } from "../"
+import getCardTypes from "../../utils/GetCardTypes";
+import { DeckWrapper, CardTypeSection, CardList, CardLine, Cost } from "./styled"
 
 const CurrentDeck = () => {
-  const { deckBuild } = useContext(AppContext)
+  const { deckBuild } = useContext(AppContext);
 
+  let cardTypes = getCardTypes(deckBuild).sort();
+
+  console.log(cardTypes);
+  console.log(deckBuild);
   return (
-    <div>
-      <ul>
-        {
-          deckBuild.map(card => (
-            <li key={card.id}>
-              {card.count}
-              {card.name}
-            </li>
-          ))
+    <DeckWrapper>
+      {cardTypes?.map((type, i) => {
+        const typePresent = deckBuild.filter((card) =>
+          card.type_line.includes(type)
+        );
+        if (typePresent?.length) {
+          return (
+            <CardTypeSection key={i}>
+              <h2>{type}</h2>
+              <CardList>
+                {typePresent?.sort((a, b) => a.cmc > b.cmc).map((card) => (
+                  <CardLine key={card.id}>
+                    <span>{card.count}</span>
+                    <span>{card.name}</span>
+                    <Cost><CastingCost card={card} /></Cost>
+                  </CardLine>
+                  ))
+                }
+              </CardList>
+            </CardTypeSection>
+          )
+        } else {
+          return null
         }
-      </ul>
-    </div>
-  )
-}
+      })}
+    </DeckWrapper>
+  );
+};
 
 export default CurrentDeck;
