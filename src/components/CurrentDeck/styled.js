18a09c67f6db963a44055229464b3d2f2df09930
Lazaro <lquin008@gmail.com>
2020-09-11 23:54:13 -0400

    CurrentDeck styled component initial

---
+++ src/components/CurrentDeck/styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const DeckWrapper = styled.main`
+  padding: 2rem
+`
+
+export const CardTypeSection = styled.section`
+`;
+
+export const CardList = styled.ul`
+`;
+
+export const CardLine = styled.li`
+  display: flex;
+  justify-content: space-between;
+  width: 25rem
+`;
+
+export const Cost = styled.div`
+  display: flex;
+  img{
+    width: 1rem;
+    height: 1rem
+  }
+`
