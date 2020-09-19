efb905ed41209c69d3585d01f07893ee9b8649e0
Lazaro <lquin008@gmail.com>
2020-09-19 15:46:12 -0400

    updated styles for line item spacing

--- src/components/CurrentDeck/styled.js
+++ src/components/CurrentDeck/styled.js
@@ -6,18 +6,27 @@ export const DeckWrapper = styled.main`
 
 export const CardTypeSection = styled.section``;
 
-export const CardList = styled.ul``;
+export const CardList = styled.ul`
+  list-style:none;
+  padding: 0;
+`;
 
 export const CardLine = styled.li`
   display: flex;
-  width: 15rem;
+  width: 20rem;
   padding: 0.5rem;
+  a{
+    flex: 8;
+  }
 `;
 
-export const CardCount = styled.span``;
+export const CardCount = styled.span`
+  flex: 0.5;
+`;
 
 export const Cost = styled.div`
   display: flex;
+  flex: 3;
   img {
     width: 1rem;
     height: 1rem;
