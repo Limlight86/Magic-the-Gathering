6058ca0037c8b35633effb6b1752fb26013e84ac
Lazaro <lquin008@gmail.com>
2020-09-22 02:15:58 -0400

    added code for handling pathing in s3 static site host

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createBrowserHistory } from "history";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory();
+
+let app = document.getElementById("root");
+if (app) {
+  const path = (/#!(\/.*)$/.exec(window.location.hash) || [])[1];
+  if (path) {
+    history.replace(path);
+  }
+}
+
+/* the above code is a work around for react router and s3 static site deployment in conjunction with the
+following instructions https://viastudio.com/hosting-a-reactjs-app-with-routing-on-aws-s3/ */
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
