053033b7e8eff0ad1db73a231c53b5de211b9ab8
Lazaro <lquin008@gmail.com>
2020-09-30 23:14:01 -0400

    added mutation for saving current deck

--- src/data/Decks.js
+++ src/data/Decks.js
@@ -5,13 +5,24 @@ import {
   createHttpLink,
 } from "@apollo/client";
 
-export const USER_DECKS_URL = "http://localhost:4000/dev/graphql";
+export const USER_DECKS_URL = process.env.REACT_APP_GATEWAY_URL;
 
 export const USER_DECKS_QUERY = gql`
   query Decks {
     decks {
       id
       deckName
+    }
+  }
+`;
+
+export const SAVE_DECK_MUTATION = gql`
+  mutation createDeck($deckName: String!, $deckContents: String!) {
+    createDeck(deckName: $deckName, deckContents: $deckContents) {
+      id
+      userId
+      deckName
+      deckContents
     }
   }
 `;
