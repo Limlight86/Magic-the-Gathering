d91456fcabf2370f791a6cddbd0ec1b0580aaec5
Lazaro <lquin008@gmail.com>
2020-10-01 16:20:47 -0400

    context file routing

--- src/context/index.js
+++ src/context/index.js
@@ -1,2 +1,4 @@
 export { AppContext, AppContextProvider }  from "./AppContext";
 export { SanityContext, SanityContextProvider }  from "./SanityContext";
+export { AuthContext, AuthContextProvider }  from "./Authentication";
+export { AuthorizationContext, AuthorizationContextProvider }  from "./Authorization";
